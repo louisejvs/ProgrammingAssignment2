8e7680ca506911d877e910cf1e74de3e2ee9339b
louisejvs <87113375+louisejvs@users.noreply.github.com>
2021-07-08 20:00:33 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Peer homework for Rprogramming week 3 in coursera.
+## I used the variable "jlvs" throughout the whole program.
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix <- function(x=matrix() generates a unique "matrix" object that can store its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  jlvs <- NULL
+  set <- function(y){
+    x <<- y
+    jlvs <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) jlvs <<- inverse
+  getInverse <- function() jlvs 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## The inverse of the special "matrix" returned by makeCacheMatrix is computed with this function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  jlvs <- x$getInverse()
+  if(!is.null(jlvs)){
+    message("getting cached data")
+    return(jlvs)
+  }
+  mat <- x$get()
+  jlvs <- solve(mat,...)
+  x$setInverse(jlvs)
+  j
 }
